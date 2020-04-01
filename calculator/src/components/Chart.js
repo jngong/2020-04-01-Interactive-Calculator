7ddadad9be8f46acf1ec6da1592443ceb3ea397e
jngong <34215359+jngong@users.noreply.github.com>
2020-04-01 14:26:02 -0400

    chart component set up with basic styling

--- calculator/src/components/Chart.js
+++ calculator/src/components/Chart.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React from "react";
 
-const Chart = ({savingsArray, interestRate, showChart}) => {
+const Chart = ({ savingsArray, interestRate, showChart }) => {
+  while (!showChart) {
+    return null;
+  }
 
-    while (!showChart) {
-        return null
-    }
+  const initialSavings = savingsArray[0];
 
-    const initialSavings = savingsArray[0]
+  const mapSavings = savingsArray.map((amount, i) => {
+    return (
+      <tr>
+        <td>{i}</td>
+        <td>${amount}</td>
+      </tr>
+    );
+  });
 
-    return(
-        <div className="chart">
-            <h2>Savings Chart</h2>
-            <p>Initial Savings: ${initialSavings}</p>
-            <p>Growth Rate: {interestRate}%</p>
-        </div>
-    )
-}
+  return (
+    <div className="chart">
+      <div className="chart-header">
+        <h2>Savings Chart</h2>
+        <p>Initial Savings: ${initialSavings}</p>
+        <p>Growth Rate: {interestRate}%</p>
+      </div>
+      <table>
+        <tr>
+          <th>Year</th>
+          <th>Savings Value</th>
+        </tr>
+        {mapSavings}
+      </table>
+    </div>
+  );
+};
 
-export default Chart
+export default Chart;
